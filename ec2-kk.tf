5141dc85670141cce01a1962b88ce9b9e228b92e
Kalyani2004 <100771557+Kalyani2004@users.noreply.github.com>
2024-05-09 18:13:55 +0530

    Delete ec2-kk.tf

--- ec2-kk.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "instance-tca1" {
-  ami           = "ami-013e83f579886baeb"
-  instance_type = "t2.micro"
-  key_name      = "KP1-Mumbai"
-  tags= {
-    name = "instance-tca1"
-    Env  = "dev"
-  }
-}
