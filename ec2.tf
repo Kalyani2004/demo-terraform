61fa1627db37eb5750dd32a6d7a126993cb03042
Kalyani2004 <100771557+Kalyani2004@users.noreply.github.com>
2024-05-09 13:11:22 +0530

    Update ec2.tf
    Updated

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "linux" {
   instance_type = "t2.micro"
   key_name      = "ram_aws"
   tags = {
-    Name = "Test_server"
+    Name = "kalyani"
     source = "terraform"
   }
 }
